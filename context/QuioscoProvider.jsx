a0949ef2686d21dfea67f4fce5ae1a256a0ea7d8
xandro2021 <xandrocastro2021@gmail.com>
2023-11-09 13:51:25 -0600

    Crear la barra lateral y mostrar las categorias

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -1,15 +1,28 @@
 import { useState, useEffect, createContext } from 'react';
+import axios from 'axios';
 
 const QuioscoContext = createContext();
 
 const QuioscoProvider = ({ children }) => {
+  const [categorias, setCategorias] = useState([]);
+
+  useEffect(() => {
+    const obtenerCategorias = async () => {
+      const { data } = await axios('/api/categorias');
+      setCategorias(data);
+    };
+
+    obtenerCategorias();
+  }, []);
+
+
   return (
     <QuioscoContext.Provider
       value={{
-
+        categorias,
       }}
     >
-      { children }
+      {children}
     </QuioscoContext.Provider>
   );
 };
