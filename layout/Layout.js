34bf8348e14becd74b3f9a667381ef1bab71d756
xandro2021 <xandrocastro2021@gmail.com>
2023-11-09 18:11:51 -0600

    actualizar layout para hacer un poco de separacion

--- layout/Layout.js
+++ layout/Layout.js
@@ -15,7 +15,9 @@ const Layout = ({ children, pagina }) => {
         </aside>
 
         <main className="md-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
-          {children}
+          <div className="p-10">
+            {children}
+          </div>
         </main>
       </div>
     </>
