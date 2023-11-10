eec25dc0042c3893c20f96d7db1da43f5fef74a6
xandro2021 <xandrocastro2021@gmail.com>
2023-11-09 18:13:37 -0600

    Seleccionar una categoria por defecto al cargar

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -5,21 +5,34 @@ const QuioscoContext = createContext();
 
 const QuioscoProvider = ({ children }) => {
   const [categorias, setCategorias] = useState([]);
+  const [categoriaActual, setCategoriaActual] = useState({});
 
   useEffect(() => {
     const obtenerCategorias = async () => {
       const { data } = await axios('/api/categorias');
       setCategorias(data);
+      // setCategoriaActual(data[0]); /*Alternativa al segund useEffect*/
     };
 
     obtenerCategorias();
   }, []);
 
+  useEffect(() => {
+    setCategoriaActual(categorias[0]);
+  }, [categorias]);
+
+
+  const handleClickCategoria = id => {
+    const [ categoria ] = categorias.filter(categoriaStatement => categoriaStatement.id === id );
+    setCategoriaActual(categoria);
+  };
 
   return (
     <QuioscoContext.Provider
       value={{
         categorias,
+        categoriaActual,
+        handleClickCategoria,
       }}
     >
       {children}
