5d1f46187613a9c0d1e593b25d99eaedc0b443cf
xandro2021 <xandrocastro2021@gmail.com>
2023-11-10 19:35:23 -0600

    Controlar la logica del pedido
    + Se usa desestructuracion en el parametro de la funcion para filtrar
    las propiedades innecesarias del objeto a almacenar en el pedido
    + Se usa un estructura condional de if para verificar si el producto a
    almacenar es nuevo o ya se encontraba en el pedido
    + Si es nuevo entonces se almacena al final del arreglo pedido
    + Si es viejo, entonces se recorre el arreglo pedido para identificar el
    elemento con su id, utilizando un map para actualizar dicho objeto
    + Despues de modificarlo de modifica el state de pedido
    + Finalmente se cierra el modal al cambiar su state a false

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -38,6 +38,21 @@ const QuioscoProvider = ({ children }) => {
     setModal(!modal);
   };
 
+  const handleAgregarPedido = ({ categoriaId, imagen, ...producto }) => {
+
+    if (pedido.some(productoState => productoState.id === producto.id)) {
+
+      const pedidoActualizado = pedido.map(productoState => productoState.id === producto.id ? producto : productoState);
+
+      setPedido(pedidoActualizado);
+    }
+    else {
+      setPedido([...pedido, producto]);
+    }
+
+    setModal(false);
+  };
+
   return (
     <QuioscoContext.Provider
       value={{
