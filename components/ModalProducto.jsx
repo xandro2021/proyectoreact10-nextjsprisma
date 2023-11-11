84a092d18efd99411666d484e7b98bc88bd55918
xandro2021 <xandrocastro2021@gmail.com>
2023-11-10 19:41:11 -0600

    registrar el evento para agregar el pedido

--- components/ModalProducto.jsx
+++ components/ModalProducto.jsx
@@ -76,6 +76,17 @@ const ModalProducto = () => {
           </button>
         </div>
 
+        <button
+          type="button"
+          className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 mt-5 text-white font-bold uppercase rounded"
+          onClick={() => handleAgregarPedido({
+            ...producto,
+            cantidad
+          })}
+        >
+          {edicion ? 'Guardar Cambios' : 'Agregar al Pedido' }
+        </button>
+
       </div>
     </div>
   );
