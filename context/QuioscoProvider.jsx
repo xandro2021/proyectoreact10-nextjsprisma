d3be76338209d059bb8662b72126be0385f83bcd
xandro2021 <xandrocastro2021@gmail.com>
2023-11-11 12:37:58 -0600

    Usar el componente toast

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, createContext } from 'react';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const QuioscoContext = createContext();
 
@@ -45,9 +46,11 @@ const QuioscoProvider = ({ children }) => {
       const pedidoActualizado = pedido.map(productoState => productoState.id === producto.id ? producto : productoState);
 
       setPedido(pedidoActualizado);
+      toast.success('Pedido Correctamente Modificado');
     }
     else {
       setPedido([...pedido, producto]);
+      toast.success('Agregado al Pedido');
     }
 
     setModal(false);
