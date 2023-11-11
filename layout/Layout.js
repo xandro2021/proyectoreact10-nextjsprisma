2bfd546afba148bf99a025bea6907ac05b3d91cd
xandro2021 <xandrocastro2021@gmail.com>
2023-11-11 12:37:45 -0600

    Registrando el componente toast para su uso

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,8 +1,12 @@
 import Head from 'next/head';
 import Modal from 'react-modal';
+import { ToastContainer } from 'react-toastify';
 import Sidebar from '../components/Sidebar';
+import Pasos from '../components/Pasos';
 import ModalProducto from '../components/ModalProducto';
 import useQuiosco from '../hooks/useQuiosco';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 const customStyles = {
   content: {
@@ -34,6 +38,7 @@ const Layout = ({ children, pagina }) => {
 
         <main className="md-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
           <div className="p-10">
+            <Pasos />
             {children}
           </div>
         </main>
@@ -47,6 +52,8 @@ const Layout = ({ children, pagina }) => {
           <ModalProducto />
         </Modal>
       )}
+
+      <ToastContainer />
     </>
   );
 };
