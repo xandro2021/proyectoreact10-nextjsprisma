3dec6cb8d917e6d6f955c44a5c01129c85d7e507
xandro2021 <xandrocastro2021@gmail.com>
2023-11-13 21:54:53 -0600

    cambiar componente imagen

--- layout/AdminLayout.js
+++ layout/AdminLayout.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
+import Image from "next/future/image";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
