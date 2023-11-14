be895725ecd8c2f07f155e05bf01da8f02e974a7
xandro2021 <xandrocastro2021@gmail.com>
2023-11-13 21:37:55 -0600

    Crear el componente Orden para mostrar cada orden

---
+++ components/Orden.js
@@ -0,0 +1,11 @@
+export default function Orden({ orden }) {
+  const { id, nombre, total, pedido } = orden;
+
+  return (
+    <div className="border p-10 space-y-5">
+      <h3 className="text-2xl font-bold">Orden: {id}</h3>
+      <p className="text-lg font-bold">Cliente: {nombre}</p>
+
+    </div>
+  );
+}
