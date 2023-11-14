f2bb08cc3bfcdd92e23ab778a42471359ffb44a7
xandro2021 <xandrocastro2021@gmail.com>
2023-11-13 20:25:21 -0600

    Crear pagina de administracion

---
+++ pages/admin.js
@@ -0,0 +1,12 @@
+import AdminLayout from '../layout/AdminLayout';
+
+export default function Admin() {
+
+  return (
+    <AdminLayout pagina="Admin">
+      <h1 className="text-4xl font-black">Panel de Administración</h1>
+      <p className="text-2xl my-10">Administra las órdenes</p>
+
+    </AdminLayout>
+  )
+}
