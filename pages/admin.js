c6d73a0a77e1358d0123a433ad2692f0492742ba
xandro2021 <xandrocastro2021@gmail.com>
2023-11-13 21:37:27 -0600

    Hacer el fetch con SWR, y mostrar el resultado

--- pages/admin.js
+++ pages/admin.js
@@ -1,12 +1,25 @@
+import useSWR from 'swr';
+import axios from 'axios';
 import AdminLayout from '../layout/AdminLayout';
+import Orden from '../components/Orden';
 
 export default function Admin() {
+
+  const fetcher = () => axios('/api/ordenes').then(datos => datos.data);
+
+  const { data, error, isLoading } = useSWR('/api/ordenes', fetcher);
 
   return (
     <AdminLayout pagina="Admin">
       <h1 className="text-4xl font-black">Panel de Administración</h1>
       <p className="text-2xl my-10">Administra las órdenes</p>
 
+      {data && data.length ? data.map(orden => (
+        <Orden
+          key={orden.id}
+          orden={orden}
+        />
+      )) : <p>No hay Ordenes Pendientes</p>}
     </AdminLayout>
   )
 }
